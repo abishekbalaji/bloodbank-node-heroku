3e7b6dd55c57de4cfdb9d150b19de27e37d58b55
Abishek B <abishekbalaji7.151@gmail.com>
2022-12-28 08:09:55 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const url =
-  "mongodb+srv://uname:password@cluster0.ezbw2.mongodb.net/bloodbank-db?retryWrites=true&w=majority" ||
+  "mongodb+srv://admin-abishek:test123@cluster0.ezbw2.mongodb.net/bloodbank-db?retryWrites=true&w=majority" ||
   "mongodb://127.0.0.1:27017/bloodbank-db";
 
 mongoose.connect(url, {
